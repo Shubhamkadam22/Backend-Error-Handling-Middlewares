353ab14cec174b20849a358480ecde7cac41663e
Shubham Kadam <shubhamkadam22100@gmail.com>
2026-08-29 12:00:00 +0530

    wrapAsync Function for error handlaing

--- app.js
+++ app.js
@@ -34,6 +34,19 @@ app.get('/err', (req, res, next) => {
     }
 }); 
 
+// wrapAsync function 
+
+function wrapAsync(fn){
+   return function (req , res , next ) {
+    fn(req , res , next).catch((err) => next(err)); 
+
+   };
+
+}
+
+
+
+
 // Centralized Error Handler
 app.use((err, req, res, next) => {
   // Fallback values if status or message aren't set
